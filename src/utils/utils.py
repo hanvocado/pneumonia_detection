8a32c07433f80a0a8146ceeac3dd6a09913ae960
Han Nguyen <han.ntnhtc@gmail.com>
2025-12-12 18:10:38 +0700

    feat: make directories modular

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,1 +1,35 @@
-# Placehoder
+import matplotlib.pyplot as plt
+import cv2
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+def show_images_in_3x3_grid(images, title_prefix='Image', header_title=None):
+    num_images = len(images)
+    
+    fig, axes = plt.subplots(3, 3, figsize=(15, 15)) 
+    
+    if header_title:
+        fig.suptitle(header_title, fontsize=16)
+    
+    for i, ax in enumerate(axes.flat):
+        if i < num_images:
+            # Hiển thị ảnh
+            ax.imshow(images[i], cmap='gray')
+            ax.set_title(f'{title_prefix} {i+1}')
+            ax.axis('off')
+        else:
+            ax.axis('off')
+    
+    plt.tight_layout()
+    plt.show()
+
+def list_folder(folder_path):
+    list_of_files = []
+    with os.scandir(folder_path) as entries:
+        for entry in entries:
+            if entry.is_file():
+                list_of_files.append(entry.name)
+
+    return list_of_files
