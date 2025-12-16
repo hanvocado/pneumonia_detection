61cfe9d04abe52789c252b0338cf5e3f44521c74
Han Nguyen <han.ntnhtc@gmail.com>
2025-12-16 14:43:51 +0700

    fix: split_dataset

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -25,6 +25,27 @@ def show_images_in_3x3_grid(images, title_prefix='Image', header_title=None):
     plt.tight_layout()
     plt.show()
 
+def show_images_in_grid(images, title_prefix='Image', cols=3, header_title=None):
+    num_images = len(images)
+    rows = (num_images + cols - 1) // cols  # Tính số hàng cần thiết
+
+    fig, axes = plt.subplots(rows, cols, figsize=(5*cols, 5*rows)) 
+    
+    if header_title:
+        fig.suptitle(header_title, fontsize=16)
+    
+    for i, ax in enumerate(axes.flat):
+        if i < num_images:
+            # Hiển thị ảnh
+            ax.imshow(images[i], cmap='gray')
+            ax.set_title(f'{title_prefix} {i+1}')
+            ax.axis('off')
+        else:
+            ax.axis('off')
+    
+    plt.tight_layout()
+    plt.show()
+
 def list_folder(folder_path):
     list_of_files = []
     with os.scandir(folder_path) as entries:
